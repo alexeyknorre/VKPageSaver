e4c220611476a198ca16e92ff22acfaa428421a9
Alexey Knorre <a.v.knorre@gmail.com>
2016-10-23 13:07:35 +0300

    Fully functional photos downloading

--- vk_save_page/photos.py
+++ vk_save_page/photos.py
@@ -21,16 +21,19 @@ def get_albums_list(user_id, secret_key):
     for album in photos_albums_response[u'response']:
         # Remove characters that are forbidden in Windows filenames
         title = re.sub(rx, '', album[u'title'])
-        os.makedirs('{0}/{1}'.format(directory, title.encode('cp1251')))
+        folder = '{0}/{1}'.format(directory, title.encode('cp1251'))
+        if not os.path.exists(folder):
+            os.makedirs(folder)
         albums_list.append([title, album[u'aid']])
     print("Got {0} photo albums.".format(len(albums_list)))
     return albums_list
 
 def get_photos_from_album(albums_list, user_id, secret_key):
     print("Getting photos...")
-    c = 0
+    c = 1
     sizes = [u'src',u'src_big',u'src_xbig', u'src_xxbig',u'src_xxxbig']
     for album in albums_list:
+        print("{0}/{1}. Downloading {2}...".format(c,len(albums_list), album[0].encode("utf-8")))
         photos_response = requests.get("https://api.vk.com/method/photos.get?owner_id=" + user_id +
                                   "&album_id="+ str(album[1]) +"&access_token="+ secret_key).json()[u'response']
         for photo in photos_response:
@@ -40,10 +43,12 @@ def get_photos_from_album(albums_list, user_id, secret_key):
                     url = photo[size]
                 except:
                     break
-            urllib.URLopener().retrieve(url, "{0}/{1}/{2}.jpg".format(directory, album[0].strip().encode("cp1251"),
-                                                                      photo[u'pid']))
+            picture = "{0}/{1}/{2}.jpg".format(directory, album[0].strip().encode("cp1251"),
+                                                                      photo[u'pid'])
+            if not os.path.isfile(picture):
+                urllib.URLopener().retrieve(url, picture)
         c+=1
-        print("{0} albums downloaded...".format(c))
+
 
 def get_photos(user_id, secret_key):
     albums_list = get_albums_list(user_id, secret_key)
